b3635d784ee50cc315458503eb2b626c1bc9268e
GBstallman <60754019+GBstallman@users.noreply.github.com>
2020-03-11 21:17:11 +0200

    Update urls.py

--- justchat/urls.py
+++ justchat/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+from rest_framework_simplejwt import views as jwt_views
 from django.urls import path, include
 from django.views.generic import TemplateView
 from rest_framework.schemas import get_schema_view
 from rest_framework_swagger.views import get_swagger_view
 from rest_framework.schemas.coreapi import AutoSchema
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+from . import views
 
 schema_view = get_swagger_view(title='Chat API')
 
@@ -13,6 +16,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('chat/', include('chat.urls', namespace='chat')),
     path('rest-auth/', include('rest_auth.urls')),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+      path('token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
