ed5813d0bf4f876472ef3bdd5e99ade9b257ae6b
GBstallman <60754019+GBstallman@users.noreply.github.com>
2020-03-11 20:25:08 +0200

    Update settings.py

--- justchat/settings.py
+++ justchat/settings.py
@@ -111,9 +111,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
 
