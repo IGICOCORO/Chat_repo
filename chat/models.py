a6ee0408f3dbeb697a3e6c5e03dd6a8ece6f817f
G50 <GBstallman>
2020-04-29 19:21:10 +0300

    routing and consumers added

--- chat/models.py
+++ chat/models.py
@@ -5,16 +5,18 @@ User = get_user_model()
 
 
 class Contact(models.Model):
-    user = models.ForeignKey(User, related_name='friends', on_delete=models.CASCADE)
-    friends = models.ManyToManyField('self', blank=True)
-    picture = models.FileField(blank=False)
+    user = models.ForeignKey(User, related_name="friends", on_delete=models.CASCADE)
+    friends = models.ManyToManyField("self", blank=True)
+    files = models.FileField(blank=False)
 
     def __str__(self):
         return self.user.username
 
 
 class Message(models.Model):
-    contact = models.ForeignKey(Contact, related_name='messages', on_delete=models.CASCADE)
+    contact = models.ForeignKey(
+        Contact, related_name="messages", on_delete=models.CASCADE
+    )
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
@@ -23,7 +25,7 @@ class Message(models.Model):
 
 
 class Chat(models.Model):
-    participants = models.ManyToManyField(Contact, related_name='chats', blank=True)
+    participants = models.ManyToManyField(Contact, related_name="chats", blank=True)
     messages = models.ManyToManyField(Message, blank=True)
 
     def __str__(self):
